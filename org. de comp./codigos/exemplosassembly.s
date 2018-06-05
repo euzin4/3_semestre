116231717ef56caedc9e97d1cf9567476e7be18a
kevin willian <tste@outlook.com.br>
2018-06-05 14:18:00 +0000

    Update exemplosassembly.s

--- org. de comp./codigos/exemplosassembly.s
+++ org. de comp./codigos/exemplosassembly.s
@@ -4,3 +4,9 @@ inicio:
 	sub r3,r4	@subtrai r3=r3-r4
 	sub r3,2	@subtrai r3=r3-2
 	jmp fora	@pula para a linha em que está 'funçao' 'fora:' dentro do codigo jogando seu endereço para o registrador "ip"
+	jmp r10		@usando registradores
+	.org 0x4000
+	depois:
+		mov r4,r3 	@se r10 tem o valor 0x4000 esta é a proxima função a ser executada
+	@desvios condicionais cap5 slide 19
+	
