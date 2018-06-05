94cef6f7d918dcd48e00510b3c9cc525b42948b0
kevin willian <tste@outlook.com.br>
2018-06-05 14:07:36 +0000

    Update exemplosassembly.s

--- org. de comp./codigos/exemplosassembly.s
+++ org. de comp./codigos/exemplosassembly.s
@@ -1,1 +1,6 @@
-a
+inicio:
+	add r1,r2	@soma r1 e r2 e salva o resultado em r1
+	add r1,2	@soma 2 ao valor que está em r1
+	sub r3,r4	@subtrai r3=r3-r4
+	sub r3,2	@subtrai r3=r3-2
+	jmp fora	@pula para a linha em que está 'funçao' 'fora:' dentro do codigo jogando seu endereço para o registrador "ip"
